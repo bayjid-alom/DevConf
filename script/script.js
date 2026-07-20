2b2b15a9241181bae34c04ff5e4701ca9e8ffa50
bayjidalom <bayjidalom1005@gmail.com>
2026-07-20 18:12:10 +0600

    Enhance banner button hover effects

--- script/script.js
+++ script/script.js
@@ -36,3 +36,24 @@ regBtn.addEventListener("mouseleave", function () {
     regBtn.style.backgroundColor = "#1D4ED8"
     regBtn.style.transform = "scale(1)";
 })
+
+
+
+
+const regLarge = document.querySelector("#banner-section button");
+// regLarge.style.transition = "backgroun-color 0.3s ease, transform 0.3s ease"
+regLarge.style.transition = "background-color 0.3s ease, transform 0.3s ease, color 0.3s ease, border 0.3s ease";
+
+regLarge.addEventListener("mouseenter", function () {
+    regLarge.style.backgroundColor = "#000822a4"
+    regLarge.style.transform = "scale(1.03)"
+    regLarge.style.color = "white"
+    regLarge.style.border = "1px solid white"
+})
+
+regLarge.addEventListener("mouseleave", function () {
+    regLarge.style.backgroundColor = "#1D4ED8"
+    regLarge.style.transform = "scale(1)"
+    regLarge.style.color = "white"
+    regLarge.style.border = "none"
+})
